ba2679015e649a57baab3f0c7bb3e05c680995ba
Jared <jared.i.green@accenture.com>
2019-03-20 11:09:47 -0500

    Cleanup and refactor RabbitTask

--- app/src/main/java/com/example/rabbitmqresearch/MainActivity.kt
+++ app/src/main/java/com/example/rabbitmqresearch/MainActivity.kt
@@ -2,17 +2,9 @@ package com.example.rabbitmqresearch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rabbitmq.client.ConnectionFactory
-import com.rabbitmq.client.Delivery
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.io.IOException
-import java.nio.charset.Charset
-import java.util.concurrent.TimeoutException
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,30 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rabbitTask = RabbitTask()
 
-        GlobalScope.async {
-            // launch new coroutine in background and continue
-            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-            println("World!") // print after delay
-            rabbitTask.send()
-        }
-        println("Hello,") // main thread continues while coroutine is delayed
-        Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
-
-
-
         sendButton.setOnClickListener {
-
-            println("Button clicked")
             GlobalScope.async {
-                println("Sending it!")
-                rabbitTask.send()
-                println("Sent it!")
+                val message = sendMessageTextArea.text.toString()
+                rabbitTask.send(message)
             }
         }
-        recieveButton.setOnClickListener {
 
+        recieveButton.setOnClickListener {
             GlobalScope.async {
                 rabbitTask.receive()
+
             }
         }
     }
